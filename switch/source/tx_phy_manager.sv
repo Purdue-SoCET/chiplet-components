e149ba7d3ff9f77c41ad517bdb3076788885807c
Michael <mjdick@purdue.edu>
2024-10-28 14:31:56 -0400

    added data ready to phy managers

--- switch/source/tx_phy_manager.sv
+++ switch/source/tx_phy_manager.sv
@@ -5,8 +5,25 @@ module tx_phy_manager #() (
     phy_manager_if.tx_phy phy_if,
     phy_manager_if.tx_switch switch_if
 );
+
+    flit_t flit, next_flit;
+
+    always_ff @(posedge clk, negedge n_rst) begin
+        if(!n_rst) begin
+            flit <= '0;
+        end
+        else begin
+            flit <= next_flit;
+        end
+    end
+
+    always_comb begin //input flit logic
+        if(phy_if.data_ready) next_flit = phy_if.flit;
+        else next_flit = flit;
+    end
+
     always_comb begin //8b10b encode
-        
+        // run 8b10b encode with {flit.vc, flit.id, flit.req, flit.payload}
 
 
 
