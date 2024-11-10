406c34820bce61abbe201a210bca741d1859cbc8
Devin Singh <drsingh2518@icloud.com>
2024-11-10 12:16:21 -0500

    switch: add crossbar

---
+++ switch/include/crossbar_if.sv
@@ -0,0 +1,24 @@
+`ifndef CROSSBAR_IF_SV
+`define CROSSBAR_IF_SV
+
+interface crossbar_if #(
+    parameter type T,
+    parameter T RESET_VAL,
+    parameter int M,
+    parameter int N
+);
+    T [M-1:0] in;
+    logic [$clog2(N)-1:0] sel;
+    T [N-1:0] out;
+
+    modport crossbar(
+        input in, sel,
+        output out
+    );
+
+    modport switch(
+        output in, sel
+    );
+endinterface
+
+`endif
