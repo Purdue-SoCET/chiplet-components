8a9b5791dd2dc0374b72a9693af32b99a87ae0e8
Michael <mjdick@purdue.edu>
2024-11-22 12:37:14 -0500

    route compute almost

--- switch/include/route_compute_if.vh
+++ switch/include/route_compute_if.vh
@@ -3,17 +3,22 @@
 
 `include "chiplet_types_pkg.vh"
 
-interface route_compute_if;
+interface route_compute_if #(
+    parameter BUFFERS
+);
 
     import chiplet_types_pkg::*;
 
-    flit_t [buffers-1:0] in_flit;
+    flit_t [BUFFERS-1:0] in_flit;
+    logic [$clog2(BUFFERS)-1:0] buffer_sel, out_sel;
 
 
     //TODO 
     modport route(
         input in_flit,
-        output 
+        input buffer_sel,
+        output id,
+        output out_sel
     );
 endinterface
 
--- switch/source/route_compute.sv
+++ switch/source/route_compute.sv
@@ -3,34 +3,48 @@
 `include "chiplet_types_pkg.vh"
 
 module route_compute #(
-    parameter pkt_id_t NODE
+    parameter pkt_id_t NODE,
+    parameter BUFFERS
+    parameter TOTAL_NODES
 ) (
     input logic clk, n_rst,
     route_compute_if.route route_if
 );
+
     pkt_id_t id;
     node_id_t req, dest;
+    format_e format;
 
-    always_ff @(posedge clk, negedge n_rst) begin
-        if(!n_rst) begin
+    typedef struct packed {
+        logic [$clog2(BUFFERS)-1:0] out_sel;
+        node_id_t                   req;
+        node_id_t                   dest;
+    } route_lut_t;
 
-        end
-        else begin
-            
-        end
-    end
+    route_lut_t route_lut [TOTAL_NODES*TOTAL_NODES*$clog2(BUFFERS)];
+    integer i;
 
-    assign id = route_if.in_flit.id;
-    assign req = route_if.in_flit.req;
-    assign dest = node_id_t'route_if.in_flit.payload[27:23];
-
+    route_lut_t head_flit;
+    assign id = route_if.in_flit[route_if.buffer_sel].id;
+    assign head_flit.req = route_if.in_flit[route_if.buffer_sel].req;
+    assign head_flit.dest = route_if.in_flit[route_if.buffer_sel].payload[27:23];
+    assign route_if.id = id;
+    assign format = format_e'route_if.in_flit[route_if.buffer_sel].payload[31:28];
+    
 
     always_comb begin
-        if(dest == NODE) begin
+        if(format == FMT_SWITCH_CFG && dest == NODE) begin
 
         end
+        else if(dest == NODE) begin
+            route_if.out_sel = '0;
+        end
         else begin
-
+            for(i = 0; i < TOTAL_NODES*TOTAL_NODES*$clog2(BUFFERS); i++) begin
+                if(route_lut[i].req == head_flit.req && route_lut[i].dest == head_flit.dest)begin
+                    route_if.out_sel = route_lut[i].out_sel;
+                end
+            end
         end
     end
 
