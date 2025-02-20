15f96eca0cd9d0a36a34dcc9800c1887b5162952
Michael Jeffery Dick <mjdick@asicfab.ecn.purdue.edu>
2025-02-20 00:54:35 -0500

    rx fsm and fifo

--- endpoint/source/req_fifo.sv
+++ endpoint/source/req_fifo.sv
@@ -3,29 +3,65 @@
 module req_fifo#() (
     input logic clk, n_rst,
     input logic crc_valid,
-    input logic [4:0] req
+    input logic [4:0] req,
+    output logic overflow,
     bus_protocol_if.peripheral_vital bus_if
 );
 
-    logic ren;
+    logic ren, underrun, next_overflow;
+    logic [4:0] next_rdata, rdata, fifo_read, count;
+
+    typedef enum logic [1:0]
+    {
+        COUNT,
+        OVERRUN,
+        UNDERRUN,
+        REN
+    };
 
     socetlib_fifo #(.T(logic[4:0]), .DEPTH(16)) requestor_fifo (
         .CLK(clk),
         .nRST(n_rst),
         .WEN(crc_valid),
         .REN(ren),
-        .clear(),
         .wdata(req),
-        .full(),
-        .empty(),
-        .underrun(),
-        .overrun(),
-        .count(),
-        .rdata(),
+        .underrun(underrun),
+        .overrun(overflow),
+        .count(count),
+        .rdata(fifo_read),
     );
 
+always_ff @(posedge clk, negedge n_rst) begin
+    if(!n_rst) begin
+        rdata <= '0;
+    end 
+    else begin
+        rdata <= next_rdata;
+    end
+end
 
+always_comb begin
+    next_rdata = '0;
+    ren = 0;
 
+    if(bus_if.ren) begin
+        casez(bus_if.addr)
+            COUNT: begin
+                next_rdata = count;
+            end
+            OVERRUN: begin
+                next_rdata = overflow;
+            end
+            UNDERRUN: begin
+                next_rdata = underrun;
+            end
+            REN: begin
+                ren = 1'b1;
+                next_rdata = fifo_read;
+            end
+        endcase
+    end
+end
 
 
 endmodule
---
+++ endpoint/source/rx_fsm.sv
@@ -0,0 +1,91 @@
+`include "chiplet_types_pkg.vh"
+`include "switch_if.vh"
+
+module rx_fsm#(
+    parameter NUM_MSGS=4,
+    parameter TX_SEND_ADDR=32'h1004,
+    parameter DEPTH
+)(
+    input logic clk, n_rst,
+    switch_if.endpoint switch_if,
+    bus_protocol_if.protocol tx_cache_if,
+);
+    import chiplet_types_pkg::*;
+
+    typedef enum logic [2:0] {
+        IDLE, GET_LENGTH, CRC_WAIT
+    } state_e;
+
+    typedef logic [PKT_LENGTH_WIDTH-1:0] length_counter_t;
+
+    state_e state, next_state;
+    length_counter_t curr_pkt_length, next_curr_pkt_length, length, next_length;
+    logic length_clear, length_done, stop_sending;
+    logic count_enable;
+    flit_t flit;
+    // pkt_id_t curr_pkt_id, next_curr_pkt_id;
+    // long_hdr_t       long_hdr;
+    // short_hdr_t      short_hdr;
+    // msg_hdr_t        msg_hdr;
+    // resp_hdr_t       resp_hdr;
+    // switch_cfg_hdr_t switch_cfg_hdr;
+
+    socetlib_counter #(.NBITS(PKT_LENGTH_WIDTH)) length_counter (
+        .CLK(clk),
+        .nRST(n_rst),
+        .clear(length_clear),
+        .count_enable(count_enable),
+        .overflow_val(curr_pkt_length),
+        .count_out(length),
+        .overflow_flag(length_done)
+    );
+
+    always_ff @(posedge clk, negedge n_rst) begin
+        if (!n_rst) begin
+            state <= IDLE;
+            curr_pkt_length <= 0;
+            // curr_pkt_id <= 0;
+        end else begin
+            state <= next_state;
+            curr_pkt_length <= next_curr_pkt_length;
+            // curr_pkt_id <= next_curr_pkt_id;
+        end
+    end
+
+    // Next state logic
+    always_comb begin
+        next_state = state;
+        casez (state)
+            IDLE : begin
+                if(switch_if.data_ready_out[0]) begin
+                    next_state = GET_LENGTH;
+                end
+            end
+            GET_LENGTH : begin
+                next_state = CRC_WAIT;
+            end
+            CRC_WAIT : begin
+                if(length_done) begin
+                    next_state = IDLE;
+                end
+            end
+            default : begin end
+        endcase
+    end
+
+    // State output logic
+    always_comb begin
+        next_curr_pkt_length = curr_pkt_length;
+        count_enable = 0;
+        casez (state)
+            IDLE : begin end
+            GET_LENGTH : begin
+                next_curr_pkt_length = expected_num_flits(switch_if.out);
+            end
+            CRC_WAIT : begin
+                count_enable = switch_if.data_ready_out[0];
+            end
+            default : begin end
+        endcase 
+    end
+endmodule
