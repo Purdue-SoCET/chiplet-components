3e7885794062ca420c05badd029a7252ebe1e26c
LucaSimoni-314 <lucasimoni3.14@gmail.com>
2025-02-21 13:56:39 -0500

    edits

--- encdec8b10b/source/wrap_dec_8b_10b.sv
+++ encdec8b10b/source/wrap_dec_8b_10b.sv
@@ -17,7 +17,7 @@ flit_t n_flit;
 comma_sel_t n_comma_sel;
 logic [PORTCOUNT-1 :0] err_dec;
 logic [6:0] n_curr_packet_size;
-logic komma_kill;
+logic done_out_n, err_in_order;
 typedef enum logic [1:0] {LOOK_FOR_START_PACKET, LOOK_FOR_DATA_PACKET, LOOK_FOR_END_PACKET} counter_fsm;
 counter_fsm seen_start_comma, n_seen_start_comma;
 
@@ -30,22 +30,18 @@ always_ff @(posedge CLK, negedge nRST) begin
         dec_if.curr_packet_size <= '0;
         seen_start_comma <= LOOK_FOR_START_PACKET;
     end
-    else if (dec_if.done) begin
-        dec_if.done_out <= dec_if.done && ~ komma_kill;
+    else  begin
+        dec_if.done_out <= dec_if.done;
         dec_if.flit <= n_flit;
         dec_if.comma_sel <= n_comma_sel;
-        dec_if.err_out <= dec_if.err || | err_dec;
+        dec_if.err_out <= dec_if.err || (( | err_dec || err_in_order) && dec_if.done);
         dec_if.curr_packet_size <= n_curr_packet_size;
         seen_start_comma <= n_seen_start_comma ;
-    end
-    else begin
-        dec_if.done_out <= '0;
-        dec_if.err_out <= dec_if.err;
     end
 end
 
 genvar i;
-
+//instantiate 5 decoders for input data
 for (i = 0; i < PORTCOUNT; i= i +1) begin : enc_8b10b_block
     dec_8b10b dec (.data_in(dec_if.enc_flit[((i + 1) * 10 - 1):(i * 10)]),.data_out(flit_data[((i + 1) * 8 - 1):(i * 8)]),.err(err_dec[i]));
 end
@@ -58,87 +54,109 @@ msg_hdr_t msg_hdr;
 resp_hdr_t resp_hdr;
 
 //comma selection
-
 always_comb begin
     n_flit = '0;
-    komma_kill = '0;
+    done_out_n = '0;
     n_comma_sel = dec_if.comma_sel;
     long_hdr = long_hdr_t'({flit_data.payload, 32'd0});
     short_hdr = short_hdr_t'(flit_data.payload);
     msg_hdr = msg_hdr_t'(flit_data.payload);
     resp_hdr = resp_hdr_t'(flit_data.payload);
-    n_curr_packet_size = dec_if.curr_packet_size;
+    if (dec_if.done) begin
+        done_out_n = '1;
+        case (dec_if.comma_length_sel) 
+        SELECT_COMMA_1_FLIT: begin
+            done_out_n = '0;
+            case(dec_if.enc_flit.word[9:0])
+                START_COMMA:  begin 
+                    n_comma_sel = START_PACKET_SEL;
+                end
+                END_COMMA:  begin 
+                    n_comma_sel = END_PACKET_SEL;
+                end
+                default: begin
+                end
+            endcase
+        end
+        SELECT_COMMA_2_FLIT: begin
+            n_flit.vc = flit_data.payload[7];
+            n_flit.id = flit_data.payload[6:5];
+            n_flit.req = flit_data.payload[4:0];
+            case(dec_if.enc_flit.word[19:10])
+                RESEND_PACKET0_COMMA:  begin 
+                    n_comma_sel = RESEND_PACKET0_SEL;
+                end
+                RESEND_PACKET1_COMMA:  begin 
+                    n_comma_sel = RESEND_PACKET1_SEL;
+                end
+                RESEND_PACKET2_COMMA: begin     
+                    n_comma_sel = RESEND_PACKET2_SEL;
+                end
+                RESEND_PACKET3_COMMA: begin     
+                    n_comma_sel = RESEND_PACKET3_SEL;
+                end
+                NACK_COMMA: begin
+                    n_comma_sel = NACK_SEL;
+                end
+                ACK_COMMA: begin
+                    n_comma_sel = ACK_SEL;
+                end
+                default: begin
+                end
+            endcase
+        end
+        SELECT_COMMA_DATA: begin    
+            n_flit = flit_data;
+            n_comma_sel = DATA_SEL;
+        end
+        default: begin
+        end
+        endcase
+    end
+end
+
+always_comb begin
+    err_in_order = '0;
     n_seen_start_comma = seen_start_comma;
-    case (dec_if.comma_length_sel) 
-    SELECT_COMMA_1_FLIT: begin
-        n_curr_packet_size = '0;
-        komma_kill = '1;
-        case(dec_if.enc_flit.word[9:0])
-            START_COMMA:  begin 
-                n_comma_sel = START_PACKET_SEL;
+    case (seen_start_comma) 
+        LOOK_FOR_START_PACKET: begin
+            if (dec_if.enc_flit.word[9:0] == START_COMMA && dec_if.comma_length_sel == SELECT_COMMA_1_FLIT && dec_if.done) begin
                 n_seen_start_comma = LOOK_FOR_DATA_PACKET;
             end
-            END_COMMA:  begin 
-                n_comma_sel = END_PACKET_SEL;
+            else if ( dec_if.comma_length_sel == SELECT_COMMA_DATA && dec_if.done) begin
+                err_in_order = '1;
+            end
+        end
+        LOOK_FOR_DATA_PACKET:
+            if (dec_if.comma_length_sel == SELECT_COMMA_DATA && dec_if.done) begin
+                n_seen_start_comma = LOOK_FOR_END_PACKET;
+            end
+            else if ( (dec_if.comma_length_sel == SELECT_COMMA_1_FLIT || dec_if.comma_length_sel == SELECT_COMMA_2_FLIT) && dec_if.done) begin
+                err_in_order = '1;
+            end
+
+        LOOK_FOR_END_PACKET:
+            if (dec_if.comma_length_sel == SELECT_COMMA_1_FLIT && dec_if.done && dec_if.enc_flit.word[9:0] == END_COMMA) begin
                 n_seen_start_comma = LOOK_FOR_START_PACKET;
             end
-            default: begin
+            else if (( dec_if.comma_length_sel == SELECT_COMMA_2_FLIT && dec_if.done) || dec_if.comma_length_sel == SELECT_COMMA_1_FLIT && dec_if.done ) begin
+                if (dec_if.enc_flit.word[9:0] == START_COMMA) begin
+                    n_seen_start_comma = LOOK_FOR_DATA_PACKET;
+                end
+                err_in_order = '1;
             end
-        endcase
-    end
-    SELECT_COMMA_2_FLIT: begin
-        n_curr_packet_size = '0;
-        n_flit.vc = flit_data.payload[7];
-        n_flit.id = flit_data.payload[6:5];
-        n_flit.req = flit_data.payload[4:0];
-        case(dec_if.enc_flit.word[19:10])
-            RESEND_PACKET0_COMMA:  begin 
-                n_comma_sel = RESEND_PACKET0_SEL;
-            end
-            RESEND_PACKET1_COMMA:  begin 
-                n_comma_sel = RESEND_PACKET1_SEL;
-            end
-            RESEND_PACKET2_COMMA: begin     
-                n_comma_sel = RESEND_PACKET2_SEL;
-            end
-            RESEND_PACKET3_COMMA: begin     
-                n_comma_sel = RESEND_PACKET3_SEL;
-            end
-            NACK_COMMA: begin
-                n_comma_sel = NACK_SEL;
-            end
-            ACK_COMMA: begin
-                n_comma_sel = ACK_SEL;
-            end
-            default: begin
-            end
-        endcase
-    end
-    SELECT_COMMA_DATA: begin    
-        n_flit = flit_data;
-        n_comma_sel = DATA_SEL;
-        if (seen_start_comma == LOOK_FOR_DATA_PACKET) begin
-            n_curr_packet_size = expected_num_flits(flit_data.payload) - 'd1;
-        //predecode message package size if reading new packet
-        // case (flit_data.payload[31:28])
-        //     FMT_LONG_READ: n_curr_packet_size = 'd2;
-        //     FMT_SHORT_READ: n_curr_packet_size = 'd1;
-        //     FMT_LONG_WRITE: n_curr_packet_size = 'd2 + (long_hdr.length != '0 ? {1'd0, long_hdr.length} : 8'd128);
-        //     FMT_MEM_RESP: n_curr_packet_size = 'd1 + (resp_hdr.length !='0 ? {1'd0,resp_hdr.length} : 8'd128); 
-        //     FMT_MSG: n_curr_packet_size = 'd1 + (msg_hdr.length != '0 ? {1'd0,msg_hdr.length} : 8'd128); 
-        //     FMT_SWITCH_CFG: n_curr_packet_size = 'd1;
-        //     FMT_SHORT_WRITE: n_curr_packet_size = 'd1 + (short_hdr.length != '0 ? {4'd0,short_hdr.length} : 8'd16);
-        //     default: begin
-        //     end
-        // endcase
-            n_seen_start_comma = LOOK_FOR_END_PACKET;
-        end
-    end
-    default: begin
-    end
     endcase
 end
 
+always_comb begin
+    n_curr_packet_size =  dec_if.curr_packet_size;
+    if (seen_start_comma == LOOK_FOR_DATA_PACKET) begin
+        n_curr_packet_size = expected_num_flits(flit_data.payload) - 'd1;
+    end
+    else if (dec_if.comma_length_sel == SELECT_COMMA_1_FLIT || dec_if.comma_length_sel == SELECT_COMMA_2_FLIT) begin
+        n_curr_packet_size = '0;
+    end
+end
 
 
 endmodule
